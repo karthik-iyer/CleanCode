d2a4368fc95ae95bad0d21bf9b29e83a4d4556ae
Karthik Iyer <kiyerj.net@gmail.com>
2020-11-29 22:54:40 -0600

    Refactored to Mayfly variables

--- CodeLuau/Speaker.cs
+++ CodeLuau/Speaker.cs
@@ -27,12 +27,7 @@ namespace CodeLuau
 		/// <returns>speakerID</returns>
 		public RegisterResponse Register(IRepository repository)
 		{
-			int? speakerId = null;
-			bool good = false;
-			bool appr = false;
-			var ot = new List<string>() { "Cobol", "Punch Cards", "Commodore", "VBScript" };
-
-			var domains = new List<string>() { "aol.com", "prodigy.com", "compuserve.com" };
+			int? speakerId = null;					
 
 			if (!string.IsNullOrWhiteSpace(FirstName))
 			{
@@ -42,14 +37,14 @@ namespace CodeLuau
 					{
 						//put list of employers in array
 						var emps = new List<string>() { "Pluralsight", "Microsoft", "Google" };
-
+						bool good = false;
 						good = Exp > 10 || HasBlog || Certifications.Count() > 3 || emps.Contains(Employer);
 
 						if (!good)
 						{
 							//need to get just the domain from the email
 							string emailDomain = Email.Split('@').Last();
-
+							var domains = new List<string>() { "aol.com", "prodigy.com", "compuserve.com" };
 							if (!domains.Contains(emailDomain) && (!(Browser.Name == WebBrowser.BrowserName.InternetExplorer && Browser.MajorVersion < 9)))
 							{
 								good = true;
@@ -58,10 +53,12 @@ namespace CodeLuau
 
 						if (good)
 						{
+							bool appr = false;
 							if (Sessions.Count() != 0)
 							{
 								foreach (var session in Sessions)
 								{
+									var ot = new List<string>() { "Cobol", "Punch Cards", "Commodore", "VBScript" };
 									foreach (var tech in ot)
 									{
 										if (session.Title.Contains(tech) || session.Description.Contains(tech))
