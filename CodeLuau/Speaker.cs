70eaacd62d83cc6e54598bb7140ff50d7210f1cb
Karthik Iyer <kiyerj.net@gmail.com>
2020-11-29 23:37:21 -0600

    Extract Method and return early

--- CodeLuau/Speaker.cs
+++ CodeLuau/Speaker.cs
@@ -26,15 +26,13 @@ namespace CodeLuau
 		/// </summary>
 		/// <returns>speakerID</returns>
 		public RegisterResponse Register(IRepository repository)
-		{
-			int? speakerId = null;
+        {
+            int? speakerId = null;
 
             var error = ValidateData();
             if (error != null) return new RegisterResponse(error);
-           
-            var preferredEmployers = new List<string>() { "Pluralsight", "Microsoft", "Google" };
-            
-            bool speakerAppearsQualified = YearsOfExperience > 10 || HasBlog || Certifications.Count() > 3 || preferredEmployers.Contains(Employer);
+
+            bool speakerAppearsQualified = AppearsExceptional();
 
             if (!speakerAppearsQualified)
             {
@@ -120,12 +118,22 @@ namespace CodeLuau
             else
             {
                 return new RegisterResponse(RegisterError.SpeakerDoesNotMeetStandards);
-            }           
+            }
 
             //if we got this far, the speaker is registered.
             return new RegisterResponse((int)speakerId);
-		}
+        }
 
+        private bool AppearsExceptional()
+        {
+            if (YearsOfExperience > 10) return true;
+            if (HasBlog) return true;
+            if (Certifications.Count() > 3) return true;
+            var preferredEmployers = new List<string>() { "Pluralsight", "Microsoft", "Google" };
+            if (preferredEmployers.Contains(Employer)) return true;
+
+            return false ;
+        }
 
         private RegisterError? ValidateData()
         {
